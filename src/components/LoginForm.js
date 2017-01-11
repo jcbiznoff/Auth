480fa344b2ccd543c4ceb6f24b7be41ab8ffbfdb
jaychung <jay@max2.com>
2017-01-11 00:00:47 -0500

    Initial commit

---
+++ src/components/LoginForm.js
@@ -0,0 +1,28 @@
+import React, {Component} from 'react';
+import {Button, Card, CardSection, Input} from './common'
+
+class LoginForm extends Component{
+	state={email : ''};
+
+	render(){
+		return(
+			<Card> 
+				<CardSection>
+					<Input 
+						placeholder = "user@email.com"
+						label="Email"
+						value = {this.state.email}
+						onChangeText = {text => this.setState({email: text})}>
+					</Input>
+				</CardSection>
+				<CardSection/>
+				<CardSection>
+					<Button>Login</Button>
+				</CardSection>
+			</Card>
+		);
+	}
+}
+
+export default LoginForm;
+
---
+++ src/components/common/index.js
@@ -0,0 +1,5 @@
+export * from './Button' //this imports and exports Button at the same time
+export * from './Card'
+export * from './CardSection'
+export * from './Header'
+export * from './Input'
